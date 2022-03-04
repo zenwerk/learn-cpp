62f91dd17a8a6f7594396ee7ff252595f6881835
zenwerk <zenwerk@gmail.com>
2022-03-05 03:52:31 +0900

    可変長引数テンプレート

---
+++ std/variadic_template.cc
@@ -0,0 +1,23 @@
+﻿// 可変長引数テンプレート
+#include <iostream>
+
+struct Foo{};
+
+void showTypes() {
+    std::cout << std::endl;
+}
+
+template<typename H, typename ...T>
+void showTypes(const H, T ...tail) {
+    std::cout << typeid(H).name() << "\t";
+    showTypes(tail...);
+}
+
+// C++20
+//auto add(auto a, auto b) {return a + b;}
+
+int main() {
+    showTypes(1, 'a', "b", 3.14, Foo{});
+
+    return 0;
+}
