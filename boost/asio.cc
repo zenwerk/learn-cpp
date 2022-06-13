4a71966c1d42f3804cbfc4087f21f59678502b3c
zenwerk <zenwerk@gmail.com>
2022-06-14 04:43:05 +0900

    add boost example

---
+++ boost/asio.cc
@@ -0,0 +1,16 @@
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <iostream>
+
+void print(const boost::system::error_code& /*e*/) { std::cout << "Hello, world!" << std::endl; }
+
+int main() {
+    boost::asio::io_service io;
+
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(3));
+    t.async_wait(&print);
+
+    io.run();
+
+    return 0;
+}
