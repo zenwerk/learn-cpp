3c6f8163c09c0300694fe1f0cb99a41ce923bd90
zenwerk <zenwerk@gmail.com>
2022-06-26 03:32:48 +0900

    chore: cosme boost-asio example

--- boost/asio.cc
+++ boost/asio.cc
@@ -5,7 +5,7 @@
 void print(const boost::system::error_code& /*e*/) { std::cout << "Hello, world!" << std::endl; }
 
 int main() {
-    boost::asio::io_service io;
+    boost::asio::io_context io;
 
     boost::asio::deadline_timer t(io, boost::posix_time::seconds(3));
     t.async_wait(&print);
