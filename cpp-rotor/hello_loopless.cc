af9e743211a2fa51b603d185245641acaaa6d616
zenwerk <zenwerk@gmail.com>
2022-06-27 04:11:59 +0900

    add cpp-rotor (but not worked)

---
+++ cpp-rotor/hello_loopless.cc
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2019-2020 Ivan Baidakou (basiliscos) (the dot dmol at gmail dot com)
+//
+// Distributed under the MIT Software License
+//
+
+
+#include "rotor.hpp"
+#include "dummy_supervisor.h"
+#include <iostream>
+
+struct hello_actor : public rotor::actor_base_t {
+    using rotor::actor_base_t::actor_base_t;
+    void on_start() noexcept override {
+        rotor::actor_base_t::on_start();
+        std::cout << "hello world\n";
+        supervisor->do_shutdown(); // sup に shutdown メッセージを送信する
+    }
+};
+
+int main() {
+    rotor::system_context_t ctx{};
+    auto timeout = boost::posix_time::milliseconds{500};
+    auto sup = ctx.create_supervisor<dummy_supervisor_t>().timeout(timeout).finish();
+    sup->create_actor<hello_actor>().timeout(timeout).finish();
+    sup->do_process();
+    return 0;
+}
