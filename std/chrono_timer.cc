acb36481ac2daf0d3fc002b918455fd2b47e8c09
zenwerk <zenwerk@gmail.com>
2022-07-12 03:04:17 +0900

    add std::chrono timer example

---
+++ std/chrono_timer.cc
@@ -0,0 +1,81 @@
+// from: https://gist.github.com/mcleary/b0bf4fa88830ff7c882d
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <cmath>
+
+#ifdef WIN32
+#define M_PI 3.141592653589793
+#endif
+
+class Timer {
+public:
+    void start() {
+        m_StartTime = std::chrono::steady_clock::now();
+        m_bRunning = true;
+    }
+
+    void stop() {
+        m_EndTime = std::chrono::steady_clock::now();
+        m_bRunning = false;
+    }
+
+    double elapsedMilliseconds() {
+        std::chrono::time_point<std::chrono::steady_clock> endTime;
+
+        if (m_bRunning) {
+            endTime = std::chrono::steady_clock::now();
+        } else {
+            endTime = m_EndTime;
+        }
+
+        return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
+    }
+
+    double elapsedSeconds() {
+        return elapsedMilliseconds() / 1000.0;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> m_StartTime;
+    std::chrono::time_point<std::chrono::steady_clock> m_EndTime;
+    bool m_bRunning = false;
+};
+
+long fibonacci(unsigned n) {
+    if (n < 2) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+//    std::chrono::time_point<std::chrono::system_clock> start, end;
+//    start = std::chrono::system_clock::now();
+//    Timer timer;
+//    timer.start();
+//    std::cout << "f(42) = " << fibonacci(42) << '\n';
+//    timer.stop();
+//
+//    std::cout << "Time: " << timer.elapsed() << std::endl;
+//    end = std::chrono::system_clock::now();
+
+//    std::chrono::duration<double> elapsed_seconds = end-start;
+//    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+//    std::cout << "finished computation at " << std::ctime(&end_time)
+//    << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
+    Timer timer;
+    timer.start();
+    int counter = 0;
+    double test, test2;
+    while (timer.elapsedSeconds() < 2.0) {
+        counter++;
+        test = std::cos(counter / M_PI);
+        test2 = std::sin(counter / M_PI);
+    }
+    timer.stop();
+
+    std::cout << counter << std::endl;
+    std::cout << "Seconds: " << timer.elapsedSeconds() << std::endl;
+    std::cout << "Milliseconds: " << timer.elapsedMilliseconds() << std::endl;
+}
