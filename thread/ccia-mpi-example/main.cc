704d671e8b5d5b61f176a161aee461534bade53d
zenwerk <zenwerk@gmail.com>
2023-01-05 03:12:48 +0900

    reformat code

--- thread/ccia-mpi-example/main.cc
+++ thread/ccia-mpi-example/main.cc
@@ -9,39 +9,49 @@ int main() {
   BankMachine bank;
   InterfaceMachine interface_hardware;
   Atm machine(bank.get_sender(), interface_hardware.get_sender());
+
   std::thread bank_thread(&BankMachine::run, &bank);
   std::thread if_thread(&InterfaceMachine::run, &interface_hardware);
   std::thread atm_thread(&Atm::run, &machine);
+
+  // メッセージキュー
   messaging::Sender atm_queue(machine.get_sender());
+
   bool quit_pressed = false;
 
   while (!quit_pressed) {
-	char c = getchar();
-	switch (c) {
-	  case '0':
-	  case '1':
-	  case '2':
-	  case '3':
-	  case '4':
-	  case '5':
-	  case '6':
-	  case '7':
-	  case '8':
-	  case '9':atm_queue.send(digit_pressed(c));
-		break;
-	  case 'b':atm_queue.send(balance_pressed());
-		break;
-	  case 'w':atm_queue.send(withdraw_pressed(50));
-		break;
-	  case 'c':atm_queue.send(cancel_pressed());
-		break;
-	  case 'q':quit_pressed = true;
-		break;
-	  case 'i':atm_queue.send(card_inserted("acc1234"));
-		break;
-	  default:
-		std::cout << "unknown message: " << c << std::endl;
-	}
+    char c = getchar();
+    switch (c) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        atm_queue.send(digit_pressed(c));
+        break;
+      case 'b':
+        atm_queue.send(balance_pressed());
+        break;
+      case 'w':
+        atm_queue.send(withdraw_pressed(50));
+        break;
+      case 'c':
+        atm_queue.send(cancel_pressed());
+        break;
+      case 'q':
+        quit_pressed = true;
+        break;
+      case 'i':
+        atm_queue.send(card_inserted("acc1234"));
+        break;
+      default:
+        std::cout << "unknown message: " << c << std::endl;
+    }
   }
 
   bank.done();
--- thread/ccia-mpi-example/mpi.h
+++ thread/ccia-mpi-example/mpi.h
@@ -9,159 +9,188 @@
 namespace messaging {
 
 
+// Message の基底クラス
 struct MessageBase {
   virtual ~MessageBase() = default;
 };
 
+// 具体的な個別メッセージ
 template<typename Msg>
 struct WrappedMessage : MessageBase {
   Msg contents;
+
   explicit WrappedMessage(Msg const &contents_) : contents(contents_) {}
 };
 
+// Messageキュー
+// 条件変数で pop するときに empty かどうかだけチェックしているスレッドセーフ実装
 class Queue {
   std::mutex m;
   std::condition_variable c;
-  std::queue<std::shared_ptr<MessageBase> > q;
+  std::queue<std::shared_ptr<MessageBase> > q;  // 標準queue
 
 public:
   template<typename T>
   void push(T const &msg) {
-	std::lock_guard<std::mutex> lk(m);
-	q.push(std::make_shared<WrappedMessage<T> >(msg));
-	c.notify_all();
+    std::lock_guard<std::mutex> lk(m);
+    q.push(std::make_shared<WrappedMessage<T>>(msg));
+    c.notify_all();  // 全体通知
   }
 
   std::shared_ptr<MessageBase> wait_and_pop() {
-	std::unique_lock<std::mutex> lk(m);
-	c.wait(lk, [&] { return !q.empty(); });
-	auto res = q.front();
-	q.pop();
-	return res;
+    std::unique_lock<std::mutex> lk(m);
+    c.wait(lk, [&] { return !q.empty(); });  // empty でなければ先に進む
+    auto res = q.front();
+    q.pop();
+    return res;
   }
 };
 
-class CloseQueue {};
+class CloseQueue : std::exception {
+};
 
+// メソッドチェーンみたいな感じのをジェネリックにやる実装
 template<typename PreviousDispatcher, typename Msg, typename Func>
 class TemplateDispatcher {
   Queue *q;
-  PreviousDispatcher *prev;
+  PreviousDispatcher *prev; // チェーンしてきたやつ
   Func f;
-  bool chained;
+  bool chained{false};
 
+  // 別の型の組み合わせの型でもfriendしてアクセスできるようにする
   template<typename Dispatcher, typename OtherMsg, typename OtherFunc>
-  friend class TemplateDispatcher;
+  friend
+  class TemplateDispatcher;
 
+  // Queue からメッセージを待って実行
   void wait_and_dispatch() {
-	for (;;) {
-	  auto msg = q->wait_and_pop();
-	  if (dispatch(msg))
-		break;
-	}
+    for (;;) {
+      auto msg = q->wait_and_pop();  // Queueからメッセージを受信
+      if (dispatch(msg))  // メッセージの実行が成功するまで dispatch し続ける
+        break;
+    }
   }
 
+  // メッセージを処理する
   bool dispatch(std::shared_ptr<MessageBase> const &msg) {
-	if (auto *wrapper = dynamic_cast<WrappedMessage<Msg> *>(msg.get())) {
-	  f(wrapper->contents);
-	  return true;
-	} else {
-	  return prev->dispatch(msg);
-	}
+    if (auto *wrapper = dynamic_cast<WrappedMessage<Msg> *>(msg.get())) {
+      // dynamic_castが成功した場合 -> template で指定したメッセージ型なのかどうかを判断している
+      f(wrapper->contents);  // templateで渡された関数を実行
+      return true;
+    } else {
+      // dynamic_castが失敗した場合
+      return prev->dispatch(msg);  // チェーンしてきたやつのに実行を移譲する
+    }
   }
 
 public:
-  TemplateDispatcher(TemplateDispatcher const &) = delete;
-  TemplateDispatcher &operator=(TemplateDispatcher const &) = delete;
-  TemplateDispatcher(TemplateDispatcher &&other)  noexcept : q(other.q), prev(other.prev), f(std::move(other.f)), chained(other.chained) {
-	other.chained = true;
+  TemplateDispatcher(TemplateDispatcher const &) = delete; // 参照コンストラクタは削除
+  TemplateDispatcher &operator=(TemplateDispatcher const &) = delete; // 代入演算子は削除
+
+  // moveコンストラクタ
+  TemplateDispatcher(TemplateDispatcher &&other) noexcept: q(other.q), prev(other.prev), f(std::move(other.f)),
+                                                           chained(other.chained) {
+    other.chained = true;
   }
 
-  TemplateDispatcher(Queue *q_, PreviousDispatcher *prev_, Func &&f_) : q(q_), prev(prev_), f(std::forward<Func>(f_)), chained(false) {
-	prev_->chained = true;
+  // handle(OtherFunc &&of) で返される TemplateDispatcher で使われるコンストラクタ
+  TemplateDispatcher(Queue *q_, PreviousDispatcher *prev_, Func &&f_) : q(q_), prev(prev_), f(std::forward<Func>(f_)) {
+    prev_->chained = true;
   }
 
   template<typename OtherMsg, typename OtherFunc>
-  TemplateDispatcher<TemplateDispatcher, OtherMsg, OtherFunc>
-  handle(OtherFunc &&of) {
-	return TemplateDispatcher<TemplateDispatcher, OtherMsg, OtherFunc>( q, this, std::forward<OtherFunc>(of));
+  TemplateDispatcher<TemplateDispatcher, OtherMsg, OtherFunc> handle(OtherFunc &&of) {
+    // TemplateDispatcher(this.q, this, 引数で渡された関数of) を返す
+    // PreviousDispatcher -> TemplateDispatcher(this), OtherMsg -> handle<...>の部分, OtherFunc -> handleの引数
+    return TemplateDispatcher<TemplateDispatcher, OtherMsg, OtherFunc>(q, this, std::forward<OtherFunc>(of));
   }
 
   ~TemplateDispatcher() noexcept(false) {
-	if (!chained)
-	  wait_and_dispatch();
+    if (!chained)
+      wait_and_dispatch();
   }
 };
 
+// メッセージ処理の起点
 class Dispatcher {
   Queue *q;
-  bool chained;
+  bool chained{false};
 
   template<typename Dispatcher, typename Msg, typename Func>
-  friend class TemplateDispatcher;
+  friend
+  class TemplateDispatcher;
 
   [[noreturn]] void wait_and_dispatch() {
-	for (;;) {
-	  auto msg = q->wait_and_pop();
-	  dispatch(msg);
-	}
+    for (;;) {
+      auto msg = q->wait_and_pop();
+      dispatch(msg);
+    }
   }
 
-  bool dispatch(std::shared_ptr<MessageBase> const &msg) {
-	if (dynamic_cast<WrappedMessage<CloseQueue> *>(msg.get())) {
-	  throw CloseQueue();
-	}
-	return false;
+  bool dispatch(std::shared_ptr<MessageBase> const &msg) noexcept(false) {
+    if (dynamic_cast<WrappedMessage<CloseQueue> *>(msg.get())) {
+      // dynamic_cast できなかったら CloseQueue なら例外を投げる
+      // prev->dispatch の終点なので処理できないメッセージを受け取った
+      throw CloseQueue();
+    }
+    return false;
   }
 
- public:
+public:
   Dispatcher(Dispatcher const &) = delete;
-  Dispatcher &operator=(Dispatcher const &) = delete;
-  Dispatcher(Dispatcher &&other) noexcept : q(other.q), chained(other.chained) {
-	other.chained = true;
+
+  Dispatcher &operator=(Dispatcher const &) = delete; // コピー演算子は削除
+
+  // moveコンストラクタ
+  Dispatcher(Dispatcher &&other) noexcept: q(other.q), chained(other.chained) {
+    other.chained = true;
   }
 
-  explicit Dispatcher(Queue *q_) : q(q_), chained(false) {}
+  explicit Dispatcher(Queue *q_) : q(q_) {}
 
+  // handleはTemplateDispatcherを返す
   template<typename Message, typename Func>
-  TemplateDispatcher<Dispatcher, Message, Func>
-  handle(Func &&f) {
-	return TemplateDispatcher<Dispatcher, Message, Func>(
-		q, this, std::forward<Func>(f));
+  TemplateDispatcher<Dispatcher, Message, Func> handle(Func &&f) {
+    return TemplateDispatcher<Dispatcher, Message, Func>(q, this, std::forward<Func>(f));
   }
 
   ~Dispatcher() noexcept(false) {
-	if (!chained) {
-	  wait_and_dispatch();
-	}
+    if (!chained) {
+      wait_and_dispatch();
+    }
   }
 };
 
+// メッセージ送信者
 class Sender {
   Queue *q;
 
- public:
+public:
   Sender() : q(nullptr) {}
+
   explicit Sender(Queue *q_) : q(q_) {}
 
+  // Queue にメッセージを詰める
   template<typename Message>
   void send(Message const &msg) {
-	if (q)
-	  q->push(msg);
+    if (q)
+      q->push(msg);
   }
 };
 
-
+// メッセージ受信者
+// Receiver のインスタンス毎にスレッドセーフなQueueが作られる
 class Receiver {
   Queue q;
 
- public:
+public:
+  // Receiver の Queue への参照保持する Senderを生成して返す
   operator Sender() {
-	return Sender(&q);
+    return Sender(&q);
   }
 
   Dispatcher wait() {
-	return Dispatcher(&q);
+    return Dispatcher(&q);
   }
 };
 
