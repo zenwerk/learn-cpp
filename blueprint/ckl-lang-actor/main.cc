c97386a5e83975e9ff2902e7b3a4cd646c8eef5b
zenwerk <zenwerk@gmail.com>
2023-03-20 05:17:19 +0900

    actorvm_03 to blueprint-ckl-lang-actor

---
+++ blueprint/ckl-lang-actor/main.cc
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <map>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <memory>
+#include <functional>
+#include <atomic>
+
+struct PrintMessage {
+  std::string text;
+};
+
+struct StopMessage {};
+
+using Message = std::variant<PrintMessage, StopMessage>;
+
+class ActorSystem;
+
+class Actor {
+public:
+  explicit Actor(ActorSystem &actor_system)
+    : actor_system_(actor_system), is_running_(true), actor_thread_(&Actor::run, this) {}
+
+  virtual void receive(const Message &message) = 0;
+
+  void send(const std::shared_ptr<Actor> &receiver, const Message &message) {
+    if (receiver.get() != this) {
+      receiver->enqueue_message(message);
+    } else {
+      receiver->receive(message);
+    }
+  }
+
+  void stop() {
+    is_running_ = false;
+    cv_.notify_one();
+    actor_thread_.join();
+  }
+
+  void run() {
+    while (is_running_) {
+      std::unique_lock<std::mutex> lock(mutex_);
+      cv_.wait(lock, [this] { return !messages_.empty() || !is_running_; });
+
+      if (!messages_.empty()) {
+        Message message = std::move(messages_.front());
+        messages_.pop();
+        lock.unlock();
+        receive(message);
+      }
+    }
+  }
+
+  void enqueue_message(const Message &message) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    messages_.push(message);
+    cv_.notify_one();
+  }
+
+protected:
+  ActorSystem &actor_system_;
+  std::atomic<bool> is_running_;
+  std::thread actor_thread_;
+  std::queue<Message> messages_;
+  std::mutex mutex_;
+  std::condition_variable cv_;
+};
+
+class ActorSystem {
+public:
+  std::shared_ptr<Actor> spawn(const std::function<std::shared_ptr<Actor>(ActorSystem &)>& actor_constructor) {
+    std::shared_ptr<Actor> actor = actor_constructor(*this);
+    std::lock_guard<std::mutex> lock(actors_mutex_);
+    actors_[actor.get()] = actor;
+    return actor;
+  }
+
+  template<typename T, typename... Args>
+  std::shared_ptr<T> spawn(Args &&... args) {
+    static_assert(std::is_base_of<Actor, T>::value, "T must be derived from Actor");
+    std::shared_ptr<T> actor = std::make_shared<T>(*this, std::forward<Args>(args)...);
+    actors_[actor.get()] = actor;
+    return actor;
+  }
+
+
+private:
+  std::map<Actor *, std::shared_ptr<Actor>> actors_;
+  std::mutex actors_mutex_;
+};
+
+//void Actor::send(std::shared_ptr<Actor> receiver, const std::string &message) {
+//  actor_system_.send(this, receiver.get(), message);
+//}
+
+class MyActor : public Actor {
+public:
+  using Actor::Actor;
+
+  void receive(const Message &message) override {
+    std::visit([this](const auto &msg) {
+      using T = std::decay_t<decltype(msg)>;
+      if constexpr (std::is_same_v<T, PrintMessage>) {
+        std::cout << "Received message: " << msg.text << std::endl;
+      } else if constexpr (std::is_same_v<T, StopMessage>) {
+        stop();
+      }
+    }, message);
+  }
+};
+
+int main() {
+  ActorSystem actor_system;
+  auto actor1 = actor_system.spawn([](ActorSystem &actor_system) { return std::make_shared<MyActor>(actor_system); });
+  auto actor2 = actor_system.spawn<MyActor>();
+
+  actor1->send(actor2, PrintMessage{"Hello from actor 1!"});
+  actor2->send(actor1, PrintMessage{"Hello from actor 2!"});
+
+  actor1->stop();
+  actor2->send(actor2, StopMessage{});
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  return 0;
+}
