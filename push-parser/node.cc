0736d5ac3986773eae50cac92a9ed45e3a2cc488
zenwerk <zenwerk@gmail.com>
2024-03-23 07:26:31 +0900

    feat: node は生ポインタで構築する

--- push-parser/node.cc
+++ push-parser/node.cc
@@ -1,15 +1,19 @@
 #include <iostream>
 #include "node.h"
 
-void node_dump(node& n) {
+void node_dump(node& n, int depth) {
+  for (int i = 0; i < depth; i++)
+    std::cout << "  ";
+
   switch (n.type) {
-    case ND_NUM:
-      std::cout << "ND_NUM: " << n.fname << ":" << n.line << " " << static_cast<node_int&>(n).val << std::endl;
-      break;
-    case ND_BINOP:
-      std::cout << "ND_BINOP: " << n.fname << ":" << n.line << " " << static_cast<node_binop&>(n).op << std::endl;
-      node_dump(*static_cast<node_binop&>(n).lhs);
-      node_dump(*static_cast<node_binop&>(n).rhs);
-      break;
+  case ND_NUM:
+    std::cout << "ND_NUM:" << static_cast<node_int&>(n).val << std::endl;
+    break;
+  case ND_BINOP:
+    std::cout << "ND_BINOP:" << static_cast<node_binop&>(n).op << std::endl;
+    ++depth;
+    node_dump(*static_cast<node_binop&>(n).lhs, depth);
+    node_dump(*static_cast<node_binop&>(n).rhs, depth);
+    break;
   }
 }
--- push-parser/node.h
+++ push-parser/node.h
@@ -1,26 +1,34 @@
 #include <cstdint>
 #include <string>
 #include <memory>
+#include <utility>
 
 enum nd_type {
-  ND_NUM, // number
+  ND_NUM = 1, // number
   ND_BINOP, // binary operator
 };
 
 struct node {
   nd_type type;
   const std::string fname;
-  uint32_t line;
+  uint32_t line{};
+
+  node() = default;
+  explicit node(nd_type type, std::string fname = "", uint32_t line = 0) : type(type), fname(std::move(fname)), line(line) {}
 };
 
 struct node_int : node {
   int32_t val;
+
+  explicit node_int(int32_t val = 0) : node(ND_NUM), val(val) {}
 };
 
 struct node_binop : node {
   std::string op;
-  std::shared_ptr<node> lhs;
-  std::shared_ptr<node> rhs;
+  node *lhs;
+  node *rhs;
+
+  explicit node_binop(std::string op = "", node *lhs = nullptr, node *rhs = nullptr) : node(ND_BINOP), op(std::move(op)), lhs(lhs), rhs(rhs) {}
 };
 
-void node_dump(node& n);
+void node_dump(node& n, int depth = 0);
